9f99464fd814e235fc44324dc4fc824e04adf25d
oussamabenalisst <benalio248@gmail.com>
2025-10-12 12:18:28 +0100

    add rendre

---
+++ tr1/tp2/rendre.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int inverse, num, suite, x, d;
+    suite = 0;
+    num = 0;
+    do
+    {
+        suite++;
+        do
+        {
+            printf("donne le %d chiffre : ", suite);
+            scanf("%d", &x);
+        } while (x < 0 || x >= 10);
+        num = num * 10 + x;
+    } while (x != 0 && suite != 9);
+    printf("la suite est %d \n", num);
+    inverse = 0;
+    while (num != 0)
+    {
+        d = num % 10;
+        inverse = (inverse * 10) + d;
+        num /= 10;
+    }
+
+    printf("la suite inverse est %d ", inverse);
+    return 0;
+}
