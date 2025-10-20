60ff1e089908b6334ff0a110fa710867585255eb
oussamabenalisst <benalio248@gmail.com>
2025-10-20 15:27:37 +0100

    fix ex3-1

--- tr1/tp2/ex3_1.c
+++ tr1/tp2/ex3_1.c
@@ -2,88 +2,87 @@
 
 int main()
 {
-    int i, temp, x, testuniforme, un, s, choix;
-    do
+    int i, temp, x, testuniforme, un, s, choix = 0;
+    while (choix > -1)
     {
-        printf("Donner un entier positif X : ");
-        scanf("%d", &x);
-    } while (x < 0);
-    do
-    {
+        do
+        {
+            printf("\nDonner un entier positif X : ");
+            scanf("%d", &x);
+        } while (x < 0);
         printf("donne votre choix : ");
         printf("\n1.uniforme\n2.palindrome\n3.parfait\n> ");
         scanf("%d", &choix);
-    } while (choix < -1 || choix > 3);
-
-    switch (choix)
-    {
-    case 1:
-        testuniforme = 1;
-        un = x % 10;
-        temp = x / 10;
-        while (temp != 0 && testuniforme)
+        switch (choix)
         {
-            if (un == temp % 10)
+        case 1:
+            testuniforme = 1;
+            un = x % 10;
+            temp = x / 10;
+            while (temp != 0 && testuniforme)
             {
-                temp /= 10;
+                if (un == temp % 10)
+                {
+                    temp /= 10;
+                }
+                else
+                {
+                    testuniforme = 0;
+                }
+            }
+            if (testuniforme)
+            {
+                printf("le nombre %d est uniforme", x);
             }
             else
             {
-                testuniforme = 0;
+                printf("le nombre %d n'est pas uniforme", x);
             }
-        }
-        if (testuniforme)
-        {
-            printf("le nombre %d est uniforme", x);
-        }
-        else
-        {
-            printf("le nombre %d n'est pas uniforme", x);
-        }
 
-        break;
-    case 2:
-        temp = x;
-        int inverse = 0;
-        while (temp != 0)
-        {
-            inverse = (inverse * 10) + (temp % 10);
-            temp /= 10;
-        }
-        if (x == inverse)
-        {
-            printf("le nombre %d est palindrome", x);
-        }
-        else
-        {
-            printf("le nombre %d n'est pas palindrome", x);
-        }
-
-        break;
-    case 3:
-        i = 1;
-        s = 0;
-        while (i < x)
-        {
-            if (x % i == 0)
+            break;
+        case 2:
+            temp = x;
+            int inverse = 0;
+            while (temp != 0)
             {
-                s += i;
+                inverse = (inverse * 10) + (temp % 10);
+                temp /= 10;
             }
-            i++;
-        }
-        if (s == x)
-        {
-            printf("le nombre %d est parfait\n", x);
-        }
-        else
-        {
-            printf("le nombre %d n'est pas parfait\n", x);
-        }
+            if (x == inverse)
+            {
+                printf("le nombre %d est palindrome", x);
+            }
+            else
+            {
+                printf("le nombre %d n'est pas palindrome", x);
+            }
 
-        break;
-    default:
-        printf("Aucun test effectue.\n");
-        break;
-    }
+            break;
+        case 3:
+            i = 1;
+            s = 0;
+            while (i < x)
+            {
+                if (x % i == 0)
+                {
+                    s += i;
+                }
+                i++;
+            }
+            if (s == x)
+            {
+                printf("le nombre %d est parfait\n", x);
+            }
+            else
+            {
+                printf("le nombre %d n'est pas parfait\n", x);
+            }
+
+            break;
+        default:
+            printf("Aucun test effectue.\n");
+            break;
+        }
+    };
     return 0;
 }
