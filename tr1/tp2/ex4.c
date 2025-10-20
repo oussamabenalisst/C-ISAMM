a788f893dea5c1486222e6f2dd982ac694804089
oussamabenalisst <benalio248@gmail.com>
2025-10-20 16:23:50 +0100

    fix ex4

--- tr1/tp2/ex4.c
+++ tr1/tp2/ex4.c
@@ -7,15 +7,21 @@ int main()
     {
         if (j == 0)
             printf("0X|");
-        printf("%03d|", j);
+        printf("%03d ", j);
     }
+    printf("\n");
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("____");
+    }
+
     for (int i = 0; i <= 10; i++)
     {
         printf("\n");
         printf("%02d|", i);
         for (int j = 0; j <= 10; j++)
         {
-            printf("%03d|", i * j);
+            printf("%03d ", i * j);
         }
     }
     return 0;
