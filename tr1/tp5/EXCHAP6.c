f27a757ad90149639bbdc26e5c647a45e243bc32
oussamabenalisst <benalio248@gmail.com>
2025-11-27 16:09:57 +0100

    fix RXCHAP6

--- tr1/tp5/EXCHAP6.c
+++ tr1/tp5/EXCHAP6.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-//EX1
-int main() {
-    char chaines[5][100];   
-    char recherche[100];    
+// EX1
+int EX1()
+{
+    char chaines[5][100];
+    char recherche[100];
     int i, trouve = 0;
     printf("Saisir 5 chaines de caracteres :\n");
-    for(i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++)
+    {
         printf("Chaine %d : ", i + 1);
         fgets(chaines[i], 100, stdin);
         chaines[i][strcspn(chaines[i], "\n")] = '\0';
@@ -14,20 +16,23 @@ int main() {
     printf("\nSaisir une chaine a rechercher : ");
     fgets(recherche, 100, stdin);
     recherche[strcspn(recherche, "\n")] = '\0';
-    for(i = 0; i < 5; i++) {
-        if(strcmp(chaines[i], recherche) == 0) {
+    for (i = 0; i < 5; i++)
+    {
+        if (strcmp(chaines[i], recherche) == 0)
+        {
             trouve = 1;
             break;
         }
     }
-    if(trouve)
+    if (trouve)
         printf("La chaine \"%s\" existe dans le tableau.\n", recherche);
     else
         printf("La chaine \"%s\" n'existe pas dans le tableau.\n", recherche);
     return 0;
 }
-//EX2
-int main() {
+// EX2
+int main()
+{
     char chaine[200];
     char c;
     int i, compteur = 0;
@@ -36,11 +41,12 @@ int main() {
     chaine[strcspn(chaine, "\n")] = '\0';
     printf("Saisir un caractere : ");
     c = getchar();
-    for(i = 0; chaine[i] != '\0'; i++) {
-        if(chaine[i] == c)
+    for (i = 0; chaine[i] != '\0'; i++)
+    {
+        if (chaine[i] == c)
             compteur++;
     }
     printf("Le caractere '%c' apparait %d fois dans la chaine.\n", c, compteur);
-
+    EX1();
     return 0;
 }
